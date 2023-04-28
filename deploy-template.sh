b9db2d49562927654a21d99edc9eb7251fcfd670
Vinicius Carvalho dos Santos <vinicius.santos@ibratan.com.br>
2023-04-28 15:53:56 -0300

    Changing deploy-template

--- deploy-template.sh
+++ deploy-template.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 
-SECRETS=$(jq -r '. | to_entries[] | "          \(.key): \(.value)"' <(doppler secrets download --no-file))
-ENV_TEMPLATE="Environment:
+ENV_VARS="Environment:
         Variables:
           JAVA_TOOL_OPTIONS: -XX:+TieredCompilation -XX:TieredStopAtLevel=1
           TWITCH_AUTH_BASE_URL: ${TWITCH_AUTH_BASE_URL}
@@ -12,7 +11,6 @@ ENV_TEMPLATE="Environment:
           AWS__SECRET_KEY: ${AWS__SECRET_KEY}
           STAGE_URL: ${STAGE_URL}
           PROD_URL: ${PROD_URL}"
-ENV_VARS="$ENV_TEMPLATE\n$SECRETS"
 TEMPLATE=$(cat template.yaml)
 
 echo -e "${TEMPLATE/\#\$ENVIRONMENT/$ENV_VARS}"
