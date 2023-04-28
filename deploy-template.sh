0c3e06b1752176e61dfa439a7876417f4c8de49f
Vinicius Carvalho dos Santos <vinicius.santos@ibratan.com.br>
2023-04-28 11:23:24 -0300

    Changes on build

--- deploy-template.sh
+++ deploy-template.sh
@@ -2,7 +2,16 @@
 
 SECRETS=$(jq -r '. | to_entries[] | "          \(.key): \(.value)"' <(doppler secrets download --no-file))
 ENV_TEMPLATE="Environment:
-        Variables:"
+        Variables:
+          JAVA_TOOL_OPTIONS: -XX:+TieredCompilation -XX:TieredStopAtLevel=1
+          TWITCH_AUTH_BASE_URL: ${TWITCH_AUTH_BASE_URL}
+          TWITCH_HELIX_BASE_URL: ${TWITCH_HELIX_BASE_URL}
+          CLIENT_ID: ${CLIENT_ID}
+          CLIENT_SECRET: ${CLIENT_SECRET}
+          AWS__ACCESS_KEY: ${AWS__ACCESS_KEY}
+          AWS__SECRET_KEY: ${AWS__SECRET_KEY}
+          STAGE_URL: ${STAGE_URL}
+          PROD_URL: ${PROD_URL}"
 ENV_VARS="$ENV_TEMPLATE\n$SECRETS"
 TEMPLATE=$(cat template.yaml)
 
